a24afe0dbf84e143a6752ebfc9f0ef9516fc5da9
INKARAN001 <IT24102182@my.sliit.lk>
2025-10-19 08:55:19 +0530

    Delete payment directory

--- payment/run-payment.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Payment Application...
-echo.
-echo Using Maven Spring Boot plugin to run the application...
-echo.
-.\mvnw.cmd spring-boot:run -Dspring-boot.run.profiles=payment
-pause
--- payment/src/main/java/com/example/payment/respository/PaymentTransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.payment.respository;
-
-import com.example.payment.entity.PaymentTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Long> {
-}
