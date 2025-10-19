99d4f8034fb78a872a750884c503b2f3359a392b
Jadavan1279 <IT24103433@my.sliit.lk>
2025-10-19 16:08:36 +0530

    Add files via upload
    custom user detail services file uploaded

---
+++ src/main/java/com/gsc/hallbooking/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.gsc.hallbooking.service;
+
+import com.gsc.hallbooking.entity.User;
+import com.gsc.hallbooking.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final UserRepository userRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole())))
+                .disabled(user.getEnabled() != null ? !user.getEnabled() : false) // Check if account is enabled
+                .build();
+    }
+}
+
