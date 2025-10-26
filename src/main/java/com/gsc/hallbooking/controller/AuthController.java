de4a2699f1785982591db9cdc9aef11cc1a6bca8
Jadavan1279 <IT24103433@my.sliit.lk>
2025-10-26 10:06:07 +0530

    Add developer comment to AuthController
    Added developer comment for clarity on functionality.

--- src/main/java/com/gsc/hallbooking/controller/AuthController.java
+++ src/main/java/com/gsc/hallbooking/controller/AuthController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * DEVELOPER: JADAVAN
+ * CRUD: User Registration & Authentication
+ */
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
